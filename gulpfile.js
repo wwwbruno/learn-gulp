7adfef8d2dea15ad621d4d0623421012bec0f64c
Bruno Almeida <bruno@disvolvi.com>
2014-08-11 21:50:41 -0300

    Add gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,25 @@ minifyCSS = require('gulp-minify-css'),
 uglifyJS = require('gulp-uglifyjs'),
 concat = require('gulp-concat');
  
-gulp.task('default', function() {
+gulp.task('minify', function() {
 
 	gulp.src('./src/css/*.css')
 	    .pipe(concat('styles.css'))
 	    .pipe(minifyCSS())
 	    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('uglify', function() {
 
 	gulp.src('./src/js/*.js')
 	    .pipe(concat('apps.js'))
 	    .pipe(uglifyJS())
 	    .pipe(gulp.dest('./js'));
-
 });
+
+gulp.task('watch', function() {
+    gulp.watch('./src/css/*.css', ['minify']);
+    gulp.watch('./src/js/*.js', ['uglify']);
+});
+
+gulp.task('default', ['minify','uglify','watch']);
